cbe67756da7563d6515e34441483e7a8e252a8b1
Emanuel-Antonio <102038148+Emanuel-Antonio@users.noreply.github.com>
2024-04-08 19:53:25 -0300

    update files

--- cliente.py
+++ cliente.py
@@ -1,19 +1,39 @@
 import socket
+import random
 
 # Configurações do cliente
-SERVER_IP = '192.168.1.105'   #Endereço IP do servidor de destino no caso broker
-PORT = 65432                  # Porta do servidor
+SERVER_IP = '192.168.1.105'   # Endereço IP do servidor de destino, no caso, broker
+TCP_PORT = 65432              # Porta TCP do servidor
+UDP_PORT = 65433              # Porta UDP do servidor para resposta
+    
+def generate_fake_temperature():
+    # Gerar uma temperatura fictícia entre -20°C e 40°C
+    return round(random.uniform(-20, 40), 2)
 
-try:
-    # Criação do socket TCP
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        # Conecta ao servidor
-        client_socket.connect((SERVER_IP, PORT))
-        # Envia uma mensagem para o servidor
-        client_socket.sendall(b'Ola, servidor TCP')
-        # Recebe a resposta do servidor
-        data = client_socket.recv(1024)
+def main():
+    
+    try:
+        # Criação do socket TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_tcp_socket:
+            # Conecta ao servidor TCP
+            client_tcp_socket.connect((SERVER_IP, TCP_PORT))
+            # Envia uma mensagem para o servidor TCP
+            client_tcp_socket.sendall(b'Ola, servidor TCP')
+            # Recebe a resposta do servidor TCP
+            tcp_response = client_tcp_socket.recv(1024)
+            print('Resposta do servidor (TCP):', tcp_response.decode())
 
-    print('Resposta do servidor:', data.decode())
-except Exception as e:
-    print('Erro:', e)
+            # Criação do socket UDP
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_udp_socket: 
+                # Converte o número para string antes de enviar   
+                temperatura = generate_fake_temperature()
+                # # Envia a temperatura para o servidor UDP 
+                temperatura_str = str(temperatura)
+                # # Envia uma mensagem para o servidor UDP
+                client_udp_socket.sendto(temperatura_str.encode(), (SERVER_IP, UDP_PORT))
+
+    except Exception as e:
+        print('Erro:', e)
+        
+if __name__ == '__main__':
+    main()
--- servidor.py
+++ servidor.py
@@ -1,25 +1,59 @@
 import socket
+from flask import Flask, request, jsonify
+import json
+import requests
+
 
 # Configurações do servidor
-HOST = '0.0.0.0'  # Permite conexões de qualquer endereço IP
-PORT = 65432      # Porta para escutar conexões
+SERVER_IP = '192.168.1.105'  # Endereço IP do servidor, no caso, broker
+TCP_PORT = 65432             # Porta TCP do servidor
+UDP_PORT = 65433             # Porta UDP do servidor para resposta
 
-# Criação do socket TCP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    # Liga o socket ao endereço e porta especificados
-    server_socket.bind((HOST, PORT))
-    # Coloca o socket em modo de escuta
-    server_socket.listen()
-    print('Servidor TCP esperando por conexões...')
-    # Aceita conexões de clientes
-    conn, addr = server_socket.accept()
-    with conn:
-        print('Conectado por', addr)
-        while True:
-            # Recebe dados do cliente
-            data = conn.recv(1024)
-            if not data:
-                break
-            print('Mensagem recebida do cliente:', data.decode())
-            # Envia mensagem de volta para o cliente
-            conn.sendall(data)
+def main():
+    # Criação do socket TCP
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_tcp_socket:
+        # Associa o socket à porta TCP
+        server_tcp_socket.bind((SERVER_IP, TCP_PORT))
+        # Configura o socket para escutar conexões
+        server_tcp_socket.listen()
+
+        print("Servidor TCP esperando por conexões...")
+
+        # Aceita uma conexão
+        conn_tcp, addr_tcp = server_tcp_socket.accept()
+        with conn_tcp:
+            print('Conectado por TCP:', addr_tcp)
+            # Recebe dados do cliente TCP
+            data_tcp = conn_tcp.recv(1024)
+            print('Mensagem recebida do cliente TCP:', data_tcp.decode())
+
+    # Criação do socket UDP
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_udp_socket:
+        # Associa o socket à porta UDP
+        server_udp_socket.bind((SERVER_IP, UDP_PORT))
+
+        print("Servidor UDP esperando por mensagens...")
+
+        # Recebe dados do cliente UDP
+        data_udp, addr_udp = server_udp_socket.recvfrom(1024)
+        print('Conectado por TCP:', addr_udp)
+        print('Mensagem recebida do cliente UDP:', data_udp.decode())
+
+        # Salva a mensagem UDP em um arquivo JSON
+        mensagem_udp = data_udp.decode()
+        with open('mensagem_udp.json', 'w') as file:
+            json.dump({'mensagem_udp': mensagem_udp}, file)
+            
+        # Carregar os dados do arquivo JSON
+        with open('mensagem_udp.json', 'r') as file:
+            data = json.load(file)
+
+        # URL da API para enviar os dados
+        url = 'http://127.0.0.1:5000/publish'
+
+        # Fazer uma solicitação POST para a API com os dados do arquivo JSON
+        response = requests.post(url, json=data) 
+        print(response)   "
+            
+if __name__ == '__main__':
+    main()
