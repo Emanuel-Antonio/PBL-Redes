c93b292a1d7115da3494520c3b688645ca127da6
Emanuel-Antonio <102038148+Emanuel-Antonio@users.noreply.github.com>
2024-04-15 18:05:06 -0300

    update broker and dispositivo

--- broker.py
+++ broker.py
@@ -1,4 +1,7 @@
-import socket, json, requests, threading
+import socket
+import json
+import requests
+import threading
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -34,22 +37,36 @@ def tcp_server():
 
     # Criação do socket TCP
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_tcp_socket:
-        # Associa o socket à porta TCP
-        server_tcp_socket.bind((SERVER_IP, TCP_PORT))
-        # Configura o socket para escutar conexões
-        server_tcp_socket.listen()
+        try:
+            # Associa o socket à porta TCP
+            server_tcp_socket.bind((SERVER_IP, TCP_PORT))
+            # Configura o socket para escutar conexões
+            server_tcp_socket.listen()
 
-        print("Servidor TCP esperando por conexões...")
+            print("Servidor TCP esperando por conexões...")
 
-        # Loop infinito para aceitar conexões
-        while True:
-            # Aceita uma conexão
-            conn_tcp, addr_tcp = server_tcp_socket.accept()
-            with conn_tcp:
-                print('Conectado por TCP:', addr_tcp)
-                # Recebe dados do cliente TCP
-                data_tcp = conn_tcp.recv(1024)
-                print('Mensagem recebida do cliente TCP:', data_tcp.decode())
+            # Loop infinito para aceitar conexões
+            i = 0
+            while True:
+                # Aceita uma conexão
+                conn_tcp, addr_tcp = server_tcp_socket.accept()
+                with conn_tcp:
+                    print('Conectado por TCP:', addr_tcp)
+                    # Recebe dados do cliente TCP
+                    data_tcp = conn_tcp.recv(1024)
+                    print('Mensagem recebida do cliente TCP:', data_tcp.decode())
+                    
+                    i += 1
+                    # Envia uma mensagem de volta para o cliente
+                    if i == 15:
+                        message_to_send = "desligar"
+                        conn_tcp.sendall(message_to_send.encode())
+                    if i == 20:
+                        message_to_send = "ligar"
+                        conn_tcp.sendall(message_to_send.encode())
+                        
+        except Exception as e:
+            pass
 
 def udp_server():
     # Configurações do servidor
@@ -58,67 +75,72 @@ def udp_server():
 
     # Criação do socket UDP
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_udp_socket:
-        # Associa o socket à porta UDP
-        server_udp_socket.bind((SERVER_IP, UDP_PORT))
+        try:
+            # Associa o socket à porta UDP
+            server_udp_socket.bind((SERVER_IP, UDP_PORT))
 
-        print("Servidor UDP esperando por mensagens...")
+            print("Servidor UDP esperando por mensagens...")
 
-        # Loop infinito para receber mensagens
-        while True:
-            # Recebe dados do cliente UDP
-            data_udp, addr_udp = server_udp_socket.recvfrom(1024)
-            print('Conectado por UDP:', addr_udp)
-            print('Mensagem recebida do cliente UDP:', data_udp.decode())
-            
-            # Envia os dados UDP para a API
-            enviar_para_api(data_udp)
+            # Loop infinito para receber mensagens
+            while True:
+                # Recebe dados do cliente UDP
+                data_udp, addr_udp = server_udp_socket.recvfrom(1024)
+                print('Conectado por UDP:', addr_udp)
+                print('Mensagem recebida do cliente UDP:', data_udp.decode())
 
+                # Envia os dados UDP para a API
+                enviar_para_api(data_udp)
+        except Exception as e:
+            pass
+        
 def enviar_para_api(data_udp):
     url_publish = 'http://127.0.0.1:8081/publicar'
     url_consume = 'http://127.0.0.1:8081/consumir'  # Rota para consumir a API
 
-    # Preparar os dados para publicar na API
-    payload = {'message': data_udp.decode()}  # Supondo que data_udp é uma sequência de bytes
-    json_payload = json.dumps(payload)  # Convertendo para JSON
-    headers = {'Content-Type': 'application/json'}
+    try:
+        # Preparar os dados para publicar na API
+        payload = {'message': data_udp.decode()}  # Supondo que data_udp é uma sequência de bytes
+        json_payload = json.dumps(payload)  # Convertendo para JSON
+        headers = {'Content-Type': 'application/json'}
 
-    # Publicar na API
-    response_publish = requests.post(url_publish, data=json_payload, headers=headers)
-    
-    # Verificar se a publicação foi bem-sucedida
-    if response_publish.status_code == 201:
-        print("Dados UDP enviados com sucesso para a API.")
-        print(json_payload)
-    else:
-        print("Erro ao enviar os dados UDP para a API:", response_publish.status_code)
-        return
-    
-    # Consumir a API para obter a mensagem recém-publicada
-    response_consume = requests.get(url_consume)
-    
-    # Verificar se a mensagem foi consumida com sucesso
-    if response_consume.status_code == 200:
-        consumed_message = response_consume.json()
-        
-        print("Mensagem consumida da API:", consumed_message)
-    else:
-        print("Erro ao consumir a API:", response_consume.status_code)
+        # Publicar na API
+        response_publish = requests.post(url_publish, data=json_payload, headers=headers)
+
+        # Verificar se a publicação foi bem-sucedida
+        if response_publish.status_code == 201:
+            print("Dados UDP enviados com sucesso para a API.")
+            print(json_payload)
+        else:
+            print("Erro ao enviar os dados UDP para a API:", response_publish.status_code)
+            return
+
+        # Consumir a API para obter a mensagem recém-publicada
+        response_consume = requests.get(url_consume)
+
+        # Verificar se a mensagem foi consumida com sucesso
+        if response_consume.status_code == 200:
+            consumed_message = response_consume.json()
+
+            print("Mensagem consumida da API:", consumed_message)
+        else:
+            print("Erro ao consumir a API:", response_consume.status_code)
+    except Exception as e:
+        print('Erro ao enviar/receber dados para/de API:', e)
 
 def main():
-    
+
     try:
         # Inicia os servidores TCP e UDP em threads separadas
         tcp_thread = threading.Thread(target=tcp_server)
         udp_thread = threading.Thread(target=udp_server)
         tcp_thread.start()
         udp_thread.start()
-        
+
         # Inicia a aplicação Flask
         app.run(port=8081, debug=True)
-        
+
     except Exception as e:
         print('Erro:', e)
-    
+
 if __name__ == '__main__':
     main()
-    
--- dispositivo.py
+++ dispositivo.py
@@ -1,21 +1,37 @@
 import socket
 import random
 import time
+import threading
 
 
 # Configurações do cliente
 SERVER_IP = '192.168.1.105'   # Endereço IP do servidor de destino, no caso, broker
 TCP_PORT = 65432              # Porta TCP do servidor
 UDP_PORT = 65433              # Porta UDP do servidor para resposta
+MENSAGE = ''
+
     
 def generate_fake_temperature():
     # Gerar uma temperatura fictícia entre -20°C e 40°C
     return round(random.uniform(-20, 40), 2)
 
+def menu():
+    global MENSAGE
+    while True:
+        print("(1) Ligar\n (2) Desligar\n")
+        n = int(input())
+        if(n == 1):
+            MENSAGE = "ligar"
+        else:
+            MENSAGE = "desligar"
+
 def main():
-    
-    try:
-        while True:
+    menu_thread = threading.Thread(target=menu)
+    menu_thread.start()
+    global MENSAGE
+    while True:
+        try:
+            
             # Criação do socket TCP
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_tcp_socket:
                 # Conecta ao servidor TCP
@@ -25,7 +41,11 @@ def main():
                 # Recebe a resposta do servidor TCP
                 tcp_response = client_tcp_socket.recv(1024)
                 print('Resposta do servidor (TCP):', tcp_response.decode())
-
+                if(tcp_response.decode() == "desligar" or tcp_response.decode() == "ligar"):
+                    MENSAGE = tcp_response.decode()
+            if MENSAGE == "desligar":
+                pass
+            else:    
                 # Criação do socket UDP
                 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_udp_socket: 
                     # Converte o número para string antes de enviar   
@@ -38,9 +58,9 @@ def main():
                     
             time.sleep(1)
 
-    except Exception as e:
-        print('Erro: Broker desconectado')
-        pass
+        except Exception as e:
+            print('Erro: Broker desconectado')
+            pass
         
 if __name__ == '__main__':
     main()
